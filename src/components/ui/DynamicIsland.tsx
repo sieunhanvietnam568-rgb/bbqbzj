7c9898d7e11e2116dbe9a7d1f79345c026882fe4
bbqbzj <user@bbqbzj.dev>
2026-05-13 22:39:36 +0800

    auto: update

--- src/components/ui/DynamicIsland.tsx
+++ src/components/ui/DynamicIsland.tsx
@@ -44,7 +44,7 @@ export default function DynamicIsland() {
             initial={{ height: 0, opacity: 0 }}
             animate={{ height: 'auto', opacity: 1 }}
             exit={{ height: 0, opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.8 }}
           >
             {navItems.map((item) => (
               <button
