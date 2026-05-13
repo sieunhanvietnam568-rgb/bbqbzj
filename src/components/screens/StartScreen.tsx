c65359a241114912c5f06b415f8bf3da85f82dd1
bbqbzj <user@bbqbzj.dev>
2026-05-13 22:32:25 +0800

    auto: update

--- src/components/screens/StartScreen.tsx
+++ src/components/screens/StartScreen.tsx
@@ -87,7 +87,7 @@ export default function StartScreen() {
       className="start-screen"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0, transition: { duration: 0.6 } }}
+      exit={{ opacity: 0, transition: { duration: 1.0 } }}
     >
       <canvas ref={canvasRef} className="start-canvas" />
 
